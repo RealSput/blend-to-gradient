0533dd285a4e2bb74e9c2c5f89c9cf0e9aec1fde
your friendly trustworthy programmer <117580169+RealSput@users.noreply.github.com>
2024-01-03 22:31:13 -0600

    make Shorter...

--- converter.py
+++ converter.py
@@ -6,15 +6,13 @@ import pyperclip
 # Set the path to store temporary OBJ and MTL files
 temp_objs_path = ""
 
-if not os.path.exists(temp_objs_path):
-    os.makedirs(temp_objs_path)
+if not os.path.exists(temp_objs_path): os.makedirs(temp_objs_path)
 
 def convert_frame_to_obj(frame_number):
     obj_path = os.path.join(temp_objs_path, f"frame_{frame_number}.obj")
     bpy.ops.wm.obj_export(
         filepath=obj_path
     )
-
     return obj_path
 
 def read_delete(path):
@@ -24,13 +22,6 @@ def read_delete(path):
         dat = data
     os.remove(path)
     return dat
-        
-def read_delete_append(obj_path, obj_list):
-    r = read_delete(obj_path)
-    obj_list.append(r)
-    
-def copy_list_to_clipboard(obj_list):
-    pyperclip.copy(json.dumps(obj_list))
 
 frame_start = bpy.context.scene.frame_start
 frame_end = bpy.context.scene.frame_end
@@ -40,11 +31,11 @@ final_res = []
 for frame_number in range(frame_start, frame_end + 1):
     bpy.context.scene.frame_set(frame_number)
     obj_path = convert_frame_to_obj(frame_number)
-    read_delete_append(obj_path, obj_list)
+    r = read_delete(obj_path)
+    obj_list.append(r)
 
 mtl_res = read_delete(os.path.join(temp_objs_path, f"frame_{frame_number}.mtl"))
 final_res.append(mtl_res)
 final_res.append(obj_list)
 
-# Copy the list to the clipboard
-copy_list_to_clipboard(final_res)
+pyperclip.copy(json.dumps(final_res))
