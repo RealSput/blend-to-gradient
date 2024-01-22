aa9ffffc6901b14a3175e504cde1e3a91325d05a
your friendly trustworthy programmer <117580169+RealSput@users.noreply.github.com>
2024-01-22 19:48:48 +0000

    Update converter.py

--- converter.py
+++ converter.py
@@ -1,19 +1,47 @@
 import json
 import bpy
 import os
-import pyperclip
 
-# Set the path to store temporary OBJ and MTL files
-temp_objs_path = ""
+def dupframe():
+    original_objects = tuple(bpy.context.scene.objects)
+    bpy.ops.object.select_all(action='DESELECT')
+    duplicated_meshes = []
+
+    def dupmesh(original_mesh):
+        if original_mesh.type == 'MESH':
+            original_mesh.select_set(True)
+            bpy.context.view_layer.objects.active = original_mesh
+            bpy.ops.object.duplicate(linked=False)
+            duplicate = bpy.context.active_object
+            duplicate.animation_data_clear()
+            duplicate.select_set(True)
+            duplicated_meshes.append(duplicate)
+
+    for obj in original_objects:
+        dupmesh(obj)
+        bpy.ops.object.select_all(action='DESELECT')
+
+    for duplicate_mesh in duplicated_meshes:
+        duplicate_mesh.animation_data_clear()
+        duplicate_mesh.select_set(True)
+
+    bpy.context.view_layer.objects.active = duplicated_meshes[0] 
+    bpy.ops.object.join()   
+
+temp_objs_path = "P:/tempobjs"
+output_file = "P:/blender-to-gradients/cell_frac_cube.json"
 
 if not os.path.exists(temp_objs_path): os.makedirs(temp_objs_path)
 
 def convert_frame_to_obj(frame_number):
     obj_path = os.path.join(temp_objs_path, f"frame_{frame_number}.obj")
+    dupframe()
     bpy.ops.wm.obj_export(
         filepath=obj_path,
-        export_triangulated_mesh=True
+        export_triangulated_mesh=True,
+        export_selected_objects=True
     )
+    bpy.ops.object.delete()
     return obj_path
 
 def read_delete(path):
@@ -39,4 +67,6 @@ mtl_res = read_delete(os.path.join(temp_objs_path, f"frame_{frame_number}.mtl"))
 final_res.append(mtl_res)
 final_res.append(obj_list)
 
-pyperclip.copy(json.dumps(final_res)) 
+res = json.dumps(final_res)
+with open(output_file, 'w') as file:
+    file.write(res)
